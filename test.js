b41e3b934eadf21deea39c63bc011f3893231398
Thatskat <109633947+Thatskat@users.noreply.github.com>
2022-09-14 15:43:57 +1000

    added variables to js

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
 // This is a test for GITHub skills
 // This is a second comment
+// What's this! A function :)
+const p = document.getElementById('Prac');
+const btn = document.getElementById('btn');
