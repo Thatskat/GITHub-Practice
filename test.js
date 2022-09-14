30718291e377aa7bddcb3169e80a299f6decf569
Thatskat <109633947+Thatskat@users.noreply.github.com>
2022-09-14 15:45:12 +1000

    added event listener to btn and function

--- test.js
+++ test.js
@@ -3,3 +3,7 @@
 // What's this! A function :)
 const p = document.getElementById('Prac');
 const btn = document.getElementById('btn');
+// Add eventlistener
+btn.addEventListener('click', function () {
+  p.innerHTML = "This is a practice on how to use GitHub tools";
+})
